e55dc70fd460f99a48fc3855125e2e40424f4eb5
olumayokun olayinka <olumayokunolayinka@gmail.com>
2024-08-19 09:56:52 +0100

    adjust reset password url and subject on notfication

---
+++ config/env.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'candidate' => [
+        'reset_password_url' => env('CANDIDATE_RESET_PASSWORD_URL'),
+    ],
+    'employer' => [
+        'reset_password_url' => env('EMPLOYER_RESET_PASSWORD_URL'),
+    ],
+];
