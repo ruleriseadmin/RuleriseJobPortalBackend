cb524108211bd3f81abbc19d64afb9fa6f72d8db
olumayokun olayinka <olumayokunolayinka@gmail.com>
2024-08-19 12:33:51 +0100

    job category for employer

---
+++ app/Http/Controllers/Domain/Employer/MetaInformationController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Domain\Employer;
+
+use App\Supports\ApiReturnResponse;
+
+class MetaInformationController extends BaseController
+{
+    public function getJobCategory()
+    {
+        return ApiReturnResponse::success([
+            [
+                'name' => 'Agriculture',
+                'uuid' => str()->uuid(),
+            ],
+        ]);
+    }
+}
