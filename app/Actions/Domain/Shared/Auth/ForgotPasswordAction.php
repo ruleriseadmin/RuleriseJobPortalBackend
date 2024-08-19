c16bc4a645fe7ecb80dd0121ebf493964cd1d277
olumayokun olayinka <olumayokunolayinka@gmail.com>
2024-08-19 09:29:16 +0100

    reset password notification

--- app/Actions/Domain/Shared/Auth/ForgotPasswordAction.php
+++ app/Actions/Domain/Shared/Auth/ForgotPasswordAction.php
@@ -4,6 +4,7 @@ namespace App\Actions\Domain\Shared\Auth;
 
 use App\Models\User;
 use App\Notifications\Domain\Shared\Auth\ForgotPasswordNotification;
+use App\Notifications\Domain\Shared\NotificationWithActionButton;
 use Exception;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\DB;
@@ -16,11 +17,17 @@ class ForgotPasswordAction
 
     protected string $token;
 
+    protected string $domain;
+
     public function execute(string $domain, User $user) : bool
     {
         $this->user = $user;
 
         $token = Crypt::encrypt($domain);//str()->random(60);
+
+        $this->domain = $domain;
+
+        $this->token = $token;
 
         DB::beginTransaction();
         try{
@@ -44,7 +51,19 @@ class ForgotPasswordAction
 
     public function sendEmail()
     {
-        //@todo sending notification
-       // Notification::send($this->user, (new ForgotPasswordNotification()));//->delay();
+        try{
+            Notification::route('mail', $this->user->email)->notify(new NotificationWithActionButton([
+                'subject' => 'Reset Password',
+                'name' => $this->user->full_name,
+                'messages' => [
+                    "You've requested for a password change, kindly click the button below to reset your password.",
+                    "If you didn't request for a password change, kindly ignore this email.",
+                ],
+                'actionText' => 'Reset Password',
+                'actionUrl' => "{$this->domain}/reset-password?token={$this->token}",
+            ]));
+        }catch(Exception $ex){
+            Log::error("Error @ ForgotPasswordAction::sendEmail : {$ex->getMessage()}");
+        }
     }
 }
---
+++ app/Notifications/Domain/Shared/NotificationWithActionButton.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Notifications\Domain\Shared;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class NotificationWithActionButton extends Notification
+{
+    use Queueable;
+
+    public array $mailData;
+
+
+    public function __construct(array $mailData)
+    {
+        $this->mailData = $mailData;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->subject(config('app.APP_NAME').': '.$this->mailData['subject']??'Notification')
+                    ->greeting('Hello '.$this->mailData['name']??'')
+                    ->lines($this->mailData['messages']??'')
+                    ->action($this->mailData['actionText']??'Click Here', $this->mailData['actionUrl']??'');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
