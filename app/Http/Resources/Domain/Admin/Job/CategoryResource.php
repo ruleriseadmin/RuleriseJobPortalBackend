b252d026f503ad2e1cbebae42c1fc39c98fa5a6d
olumayokun olayinka <olumayokunolayinka@gmail.com>
2024-09-12 14:06:08 +0100

    fix pagination

--- app/Http/Resources/Domain/Admin/Job/CategoryResource.php
+++ app/Http/Resources/Domain/Admin/Job/CategoryResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Domain\Admin\Job;
 
+use App\Traits\Domain\Shared\PaginationTrait;
 use Illuminate\Http\Request;
 use App\Supports\HelperSupport;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -9,6 +10,10 @@ use App\Http\Resources\Domain\FrontPage\JobResource;
 
 class CategoryResource extends JsonResource
 {
+    private int $perPage = 10;
+
+    use PaginationTrait;
+
     public function toArray(Request $request): array
     {
         $response = collect(parent::toArray($request))->only([
@@ -25,7 +30,7 @@ class CategoryResource extends JsonResource
 
         if ( $this->withJobs ?? false ){
             $response = $response->merge([
-                'jobs' => JobResource::collection($this->openJobs),
+                'jobs' => $this->paginateFromCollection(JobResource::collection($this->openJobs), 10),
             ]);
         }
 
