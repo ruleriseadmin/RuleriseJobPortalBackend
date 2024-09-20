915af0029927e4564e26a84971d91dfb101e1bb4
olumayokun olayinka <olumayokunolayinka@gmail.com>
2024-09-20 05:07:16 +0100

    fix error detected

--- app/Http/Controllers/Domain/Shared/AccountSetting/UserAccountSettingsController.php
+++ app/Http/Controllers/Domain/Shared/AccountSetting/UserAccountSettingsController.php
@@ -2,19 +2,21 @@
 
 namespace App\Http\Controllers\Domain\Shared\AccountSetting;
 
-use App\Http\Controllers\Domain\Candidate\BaseController;
 use Illuminate\Http\JsonResponse;
 use App\Supports\ApiReturnResponse;
 use App\Actions\Domain\Shared\AccountSetting\UploadProfilePictureAction;
 use App\Http\Requests\Domain\Shared\AccountSetting\UploadProfilePictureRequest;
 
-class UserAccountSettingsController extends BaseController
+class UserAccountSettingsController
 {
     public function uploadProfilePicture(UploadProfilePictureRequest $request): JsonResponse
     {
         $uploadLogo = (new UploadProfilePictureAction)->execute($request->input());
+
+        $user = auth()->user();
+        
         return $uploadLogo
-            ? ApiReturnResponse::success(['profilePictureUrl' => asset("/storage/{$this->user->refresh()->profile_picture_url}")])
+            ? ApiReturnResponse::success(['profilePictureUrl' => asset("/storage/{$user->refresh()->profile_picture_url}")])
             : ApiReturnResponse::failed();
     }
 }
