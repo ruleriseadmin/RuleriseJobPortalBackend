7a106c7672c16b3706f2df97b507eb54466fe3f3
olumayokun olayinka <olumayokunolayinka@gmail.com>
2024-09-20 05:04:27 +0100

    wip

--- app/Http/Controllers/Domain/Shared/AccountSetting/UserAccountSettingsController.php
+++ app/Http/Controllers/Domain/Shared/AccountSetting/UserAccountSettingsController.php
@@ -2,17 +2,19 @@
 
 namespace App\Http\Controllers\Domain\Shared\AccountSetting;
 
+use App\Http\Controllers\Domain\Candidate\BaseController;
 use Illuminate\Http\JsonResponse;
 use App\Supports\ApiReturnResponse;
 use App\Actions\Domain\Shared\AccountSetting\UploadProfilePictureAction;
 use App\Http\Requests\Domain\Shared\AccountSetting\UploadProfilePictureRequest;
 
-class UserAccountSettingsController
+class UserAccountSettingsController extends BaseController
 {
     public function uploadProfilePicture(UploadProfilePictureRequest $request): JsonResponse
     {
-        return (new UploadProfilePictureAction)->execute($request->input())
-            ? ApiReturnResponse::success()
+        $uploadLogo = (new UploadProfilePictureAction)->execute($request->input());
+        return $uploadLogo
+            ? ApiReturnResponse::success(['profilePictureUrl' => asset("/storage/{$this->user->refresh()->profile_picture_url}")])
             : ApiReturnResponse::failed();
     }
 }
