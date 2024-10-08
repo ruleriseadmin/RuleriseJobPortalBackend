ecc5328497acc92f50a1503e2bb9cc601b28fdec
olumayokun olayinka <olumayokunolayinka@gmail.com>
2024-10-08 08:38:15 +0100

    update admin user validation

--- app/Http/Requests/Domain/Admin/UserManagement/User/UpdateUserRequest.php
+++ app/Http/Requests/Domain/Admin/UserManagement/User/UpdateUserRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests\Domain\Admin\UserManagement\User;
 use App\Http\Requests\BaseRequest;
+use App\Models\Domain\Admin\AdminUser;
 use Illuminate\Validation\Rules\Password;
+use Illuminate\Contracts\Validation\Validator;
 
 class UpdateUserRequest extends BaseRequest
 {
@@ -11,10 +13,24 @@ class UpdateUserRequest extends BaseRequest
         return [
             'firstName' => ['required'],
             'lastName' => ['required'],
-            'email' => ['required', 'email', 'unique:admin_users,email'],
-            'password' => ['required', Password::min(8)->letters()->numbers()],
+            'email' => ['nullable', 'email'],
+            'password' => ['nullable', Password::min(8)->letters()->numbers()],
             'role' => ['required'],
             'userId' => ['required', 'exists:admin_users,uuid']
         ];
+    }
+
+    public function withValidator(Validator $validator)
+    {
+        $validator->after(function ($validator){
+            //insert rule inside
+            if ( $this->filled('email') && $this->filled('userId') ){
+                $user = AdminUser::whereUuid($this->input('userId'));
+
+                if ( $user->email == $this->input('email') ) return;
+
+                ! AdminUser::where('email', $this->input('email'))->exists()?:$validator->errors()->add('email', 'Email already exists');
+            }
+        });
     }
 }
