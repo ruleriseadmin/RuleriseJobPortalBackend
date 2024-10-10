88066af38ef4c4cf11d9f959fe9fde40b4ef50f7
olumayokun olayinka <olumayokunolayinka@gmail.com>
2024-10-10 13:16:03 +0100

    rectify admin user role adding permissions

--- app/Actions/Domain/Admin/UserManagement/User/UpdateUserAction.php
+++ app/Actions/Domain/Admin/UserManagement/User/UpdateUserAction.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Hash;
 use App\Models\Domain\Admin\AdminUser;
+use Spatie\Permission\Models\Role;
 
 class UpdateUserAction
 {
@@ -27,9 +28,14 @@ class UpdateUserAction
                 $user->assignRole($inputs['role']);
             }
 
-            if ( collect($inputs)->has('permissions') ) {
-                $user->syncPermissions($inputs['permissions']);
-            }
+            $permissions = Role::whereName($inputs['role'])->first()->permissions->pluck('name');
+
+            //$input['permissions'] ??
+
+            $user->syncPermissions( $permissions ?? []);
+            // if ( collect($inputs)->has('permissions') ) {
+            //     $user->syncPermissions($inputs['permissions']);
+            // }
 
             DB::commit();
 
